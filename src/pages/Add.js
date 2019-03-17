07cbb2c5343ac67c3ffda5301960c26ab932b1d1
prisan6046 <prisan_big_bboy@hotmail.com>
2019-03-17 22:54:56 +0700

    edit

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -6,27 +6,28 @@ import Corpor6Form from '../components/form/Corpor6Form';
 
 class Add extends Component {
 
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            project_id : ''
+            project_id: ''
         }
 
     }
 
-    componentDidMount(){
-        this.setState({ 
-           project_id  : localStorage.getItem('project_id')}
+    componentDidMount() {
+        this.setState({
+            project_id: localStorage.getItem('project_id')
+        }
         )
-        console.log(" project_id " ,this.state.project_id)
+        console.log(" project_id ", this.state.project_id)
     }
 
 
 
     render() {
-        return(
+        return (
             <div className="Add">
-            
+
                 <div className="container-fluid py-4">
                     <div className="row">
                         <div className="col-lg-12">
@@ -34,50 +35,63 @@ class Add extends Component {
                                 <div className="card-body">
                                     <div className="row mb-2">
                                         <div className="col-lg-12">
-                                            <ul className="nav nav-tabs">
-                                                { this.state.project_id == null ? 
-                                                 <li className="nav-item">
-                                                    <a className="nav-link active" id="corpor3-tab" data-toggle="tab" href="#corpor3" role="tab" aria-controls="corpor3" aria-selected="true">ฟอร์ม คพ.3</a>
-                                                </li>: <div></div>
-                                                }
-                                                {
-                                                    this.state.project_id != null ? 
+
+                                            {this.state.project_id == null ?
+                                                <ul className="nav nav-tabs">
+                                                    <li className="nav-item">
+                                                        <a className="nav-link active" id="corpor3-tab" data-toggle="tab" href="#corpor3" role="tab" aria-controls="corpor3" aria-selected="true">ฟอร์ม คพ.3</a>
+                                                    </li>
+                                                </ul> : <div></div>
+
+                                            }
+                                            {
+                                                this.state.project_id != null ?
                                                     <div>
-                                                    <li className="nav-item">
-                                                    <a className="nav-link" id="corpor4-tab" data-toggle="tab" href="#corpor4" role="tab" aria-controls="corpor3" aria-selected="true">ฟอร์ม คพ.4</a>
-                                                    </li>
-                                                    <li className="nav-item">
-                                                        <a className="nav-link" id="corpor5-tab" data-toggle="tab" href="#corpor5" role="tab" aria-controls="corpor3" aria-selected="true">ฟอร์ม คพ.5</a>
-                                                    </li>
-                                                    <li className="nav-item">
-                                                        <a className="nav-link" id="corpor6-tab" data-toggle="tab" href="#corpor6" role="tab" aria-controls="corpor3" aria-selected="true">ฟอร์ม คพ.6</a>
-                                                    </li>
+                                                        <ul className="nav nav-tabs">
+                                                            <li className="nav-item">
+                                                                <a className="nav-link" id="corpor4-tab" data-toggle="tab" href="#corpor4" role="tab" aria-controls="corpor3" aria-selected="true">ฟอร์ม คพ.4</a>
+                                                            </li>
+                                                            <li className="nav-item">
+                                                                <a className="nav-link" id="corpor5-tab" data-toggle="tab" href="#corpor5" role="tab" aria-controls="corpor3" aria-selected="true">ฟอร์ม คพ.5</a>
+                                                            </li>
+                                                            <li className="nav-item">
+                                                                <a className="nav-link" id="corpor6-tab" data-toggle="tab" href="#corpor6" role="tab" aria-controls="corpor3" aria-selected="true">ฟอร์ม คพ.6</a>
+                                                            </li>
+                                                        </ul>
                                                     </div>
                                                     :
-                                                <div></div>
+                                                    <div></div>
 
-                                                }
-                                               
-
-                                                
-                                            </ul>
-
+                                            }
                                             <div className="tab-content" id="myTabContent">
 
-                                                <div className="tab-pane fade show active" id="corpor3" role="tabpanel" aria-labelledby="corpor3-tab">
-                                                    <Corpor3Form />
-                                                </div>
-                                                <div className="tab-pane fade" id="corpor4" role="tabpanel" aria-labelledby="corpor4-tab">
-                                                    <Corpor4Form />
-                                                </div>
-                                                <div className="tab-pane fade" id="corpor5" role="tabpanel" aria-labelledby="corpor5-tab">
-                                                    <Corpor5Form />
-                                                </div>
-                                                <div className="tab-pane fade" id="corpor6" role="tabpanel" aria-labelledby="corpor6-tab">
-                                                    <Corpor6Form />
-                                                </div>
+                                                {
+                                                    this.state.project_id == null ?
+                                                        <div className="tab-content" id="myTabContent">
+                                                            <div className="tab-pane fade show active" id="corpor3" role="tabpanel" aria-labelledby="corpor3-tab">
+                                                                <Corpor3Form />
+                                                            </div>
+                                                        </div>
+                                                        : <div></div>
+                                                }
+                                                {
+                                                    this.state.project_id != null ?
+                                                        <div className="tab-content" id="myTabContent">
+                                                            <div className="tab-pane fade show active" id="corpor4" role="tabpanel" aria-labelledby="corpor4-tab">
+                                                                <Corpor4Form />
+                                                            </div>
+                                                            <div className="tab-pane fade" id="corpor5" role="tabpanel" aria-labelledby="corpor5-tab">
+                                                                <Corpor5Form />
+                                                            </div>
+                                                            <div className="tab-pane fade" id="corpor6" role="tabpanel" aria-labelledby="corpor6-tab">
+                                                                <Corpor6Form />
+                                                            </div>
+                                                        </div>
+                                                        :
+                                                        <div></div>
+                                                }
                                             </div>
-                                        </div>                                        
+                                        </div>
                                     </div>
                                 </div>
                             </div>
